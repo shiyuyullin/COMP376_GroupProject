55eeda6f45c23094e84b8bcac733112ccc27e31b
shiyuyullin <dayu19990519@gamil.com>
2020-10-25 13:32:17 -0400

    update

---
+++ DuangDuang/Assets/Scripts/CarController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CarController : MonoBehaviour
+{
+    //movement
+    [SerializeField] float mSpeed;
+    [SerializeField] float mAngularSpeed;
+
+    //bumper
+    [SerializeField] float mForce;
+    [SerializeField] float mBumperForce;
+    [SerializeField] float mForceRadius;
+
+    void Start()
+    {
+        mSpeed = 10f;
+        mAngularSpeed = 100f;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    //private void OnCollisionEnter(Collision collisionInfo)
+    //{
+    //    if (collisionInfo.collider.CompareTag("Dirt"))
+    //        Destroy(collisionInfo.collider.gameObject);
+    //    if (collisionInfo.collider.CompareTag("Player"))
+    //        collisionInfo.collider.attachedRigidbody.AddForce(mForce * Vector3.forward, ForceMode.Impulse);
+    //    //collisionInfo.rigidbody.AddExplosionForce(mBumperForce, transform.position, mForceRadius, 0.0f, ForceMode.Impulse);
+    //}
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * mAngularSpeed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * mSpeed * Time.deltaTime;
+
+        transform.Translate(vertical, 0, 0);
+        transform.Rotate(0, horizontal, 0);
+    }
+}
---
+++ DuangDuang/Assets/Scripts/FollowCamer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FollowCamer : MonoBehaviour
+{
+    public Transform cameraLocation;
+    public float speed = 5f;
+    public Vector3 distance;
+    public Transform targetLocation;
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraLocation.position + distance, speed * Time.deltaTime);
+        transform.LookAt(targetLocation.position);
+    }
+
+}
