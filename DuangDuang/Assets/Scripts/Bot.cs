4cda55b3adef39d49bd1f53b76c45b39a9185b35
shiyuyullin <dayu19990519@gamil.com>
2020-10-27 23:59:29 -0400

    fixing

--- DuangDuang/Assets/Scripts/Bot.cs
+++ DuangDuang/Assets/Scripts/Bot.cs
@@ -11,6 +11,9 @@ public class Bot : MonoBehaviour
     [SerializeField] private LayerMask TargetItemLayer;
     [SerializeField] private float viewrange;
     [SerializeField] private string tagToLookFor;
+    [SerializeField] float forceMagnitude;
+    [SerializeField] float recoil;
+
     private NavMeshAgent navMeshAgent;
     private float timer;
 
@@ -20,20 +23,34 @@ public class Bot : MonoBehaviour
         this.stateMachine.changeState(new SearchForTarget(this.TargetItemLayer, this.gameObject, this.viewrange, this.tagToLookFor, this.targetFound));
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         this.stateMachine.execuateStateUpdate();
     }
 
     public void targetFound(SearchResults searchResults)
     {
-        timer += Time.deltaTime;
+
         //unpack information, furthur processing the information
-        if(timer <= 3.0f)
+
+        navMeshAgent.SetDestination(searchResults.allHitObjectsWithRequiredTag[0].transform.position);
+        //navMeshAgent.SetDestination(new Vector3(0.0f,0.0f,0.0f));
+        Debug.Log(searchResults.allHitObjectsWithRequiredTag[0].transform.position);
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Obstacles") { }
+
+        if (collision.gameObject.tag == "Player")
         {
-            navMeshAgent.SetDestination(searchResults.allHitObjectsWithRequiredTag[0].transform.position);
+            Vector3 forceDirection = collision.gameObject.transform.position - gameObject.transform.position;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+            gameObject.GetComponent<Rigidbody>().AddForce(-forceDirection * recoil, ForceMode.Impulse);
         }
     }
+
 
 
 }
