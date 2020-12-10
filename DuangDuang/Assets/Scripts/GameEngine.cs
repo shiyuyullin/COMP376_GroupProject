fea6b84718603a1e1372ad07c966a021d97eeba7
Luan Meiyi <luanmeiyi@gmail.com>
2020-12-09 19:22:57 -0500

    map j

--- DuangDuang/Assets/Scripts/GameEngine.cs
+++ DuangDuang/Assets/Scripts/GameEngine.cs
@@ -22,12 +22,19 @@ public class GameEngine : MonoBehaviour
     //Map-J respawns
     GameObject[] respawns;
     string sceneName;
-
+    //Map-L 
+    public GameObject teaCup;
+    public GameObject [] teaCups = new GameObject[5];
+    float angle;
+    float r;
+    float distance=15;
     // Start is called before the first frame update
     void Start()
     {
         Scene currentScene = SceneManager.GetActiveScene();
+        
         sceneName = currentScene.name;
+        Debug.Log(sceneName);
         
         if (sceneName == "Map-J")
         {
@@ -39,6 +46,20 @@ public class GameEngine : MonoBehaviour
             GameObject friendlyBot1 = Instantiate(friendlyBots, respawns[1].transform.position, Quaternion.identity);
             GameObject friendlyBot2 = Instantiate(friendlyBots, respawns[3].transform.position, Quaternion.identity);
         }
+        if(sceneName =="Map-L"){
+            
+
+            r=distance;
+            angle=0;
+            for(int i =0 ; i<5; i++){
+                float temp=i/5.0f *2*Mathf.PI;
+                float x=distance*Mathf.Cos(temp);
+                float z=distance*Mathf.Sin(temp);
+                float y=0;
+
+                teaCups[i]= Instantiate(teaCup,new Vector3(x,y,z),Quaternion.identity);
+            }
+        }
 
         //GameObject enemyBot1 = Instantiate(enemyBots, new Vector3(13.16f, 0.0f, 4.935f),Quaternion.identity);
         //GameObject enemyBot2 = Instantiate(enemyBots, new Vector3(18.1f, 0.0f, -11.1f), Quaternion.identity);
@@ -49,6 +70,7 @@ public class GameEngine : MonoBehaviour
 
     }
 
+
     // Update is called once per frame
     void Update()
     {
@@ -56,9 +78,13 @@ public class GameEngine : MonoBehaviour
         {
             Time.timeScale = 1;
         }
+        timer += Time.deltaTime;
+        if(sceneName=="Map-L"){
+            moveTeaCup(timer);
+        }
 
         //item appear
-        timer += Time.deltaTime;
+        
         if (createdItem < totalItem)
         {
             if (timer > spawnTime)
@@ -76,8 +102,33 @@ public class GameEngine : MonoBehaviour
         {
             position = new Vector3(0, 0, 0);
         }
+        if(sceneName =="Map-L"){
+            position = new Vector3(0,0,0);
+        }
         GameObject iceCream = Instantiate(itemPrefab, position, itemPrefab.transform.rotation);
         //TODO rotate slowly
         Debug.Log("Item spawn!");
+    }
+    void moveTeaCup(float timer){
+        float temp=angle;
+        angle=angle+Time.deltaTime/2.0f;
+        if(angle > 2* Mathf.PI){
+            angle-=2*Mathf.PI;
+        }
+        r=distance*(Mathf.Cos(timer/2.0f)+1)/2.0f+5;
+        for(int i =0 ; i<5; i++){
+            
+            if(temp > 2* Mathf.PI){
+                temp-=2*Mathf.PI;
+            }
+            
+            float x=r*Mathf.Cos(temp);
+            float z=r*Mathf.Sin(temp);
+            float y=0;
+
+            teaCups[i].transform.position=new Vector3(x,y,z);
+            temp+=1/5.0f *2*Mathf.PI;;
+            }
+
     }
 }
